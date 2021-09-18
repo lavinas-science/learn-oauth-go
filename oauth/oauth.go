1fee488d248f5905e2e99f9fc429d30b5e25e07b
lavinas-science <lavinas@gmail.com>
2021-09-18 15:35:17 -0300

    First

--- oauth/oauth.go
+++ oauth/oauth.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"net/http"
 	"strings"
+	"time"
+	"strconv"
 
 	"github.com/go-resty/resty/v2"
 	"github.com/lavinas-science/learn-oauth-go/utils/errors"
@@ -12,31 +14,27 @@ import (
 const (
 	headerXPublic   = "X-Public"
 	headerXClientId = "X-Client-Id"
-	headreXCallerId = "X-Caller-Id"
-
+	headerXCallerId = "X-Caller-Id"
 	paramAccessToken = "access_token"
-
 	UserContentType = "application/json"
 	UserBaseURI = "http://127.0.0.1:9090"
 	UserURI = "/oauth/access_token"
-
+	timeoutSeconds = 1
 )
 
 var (
 	client = resty.New()
 )
 
+func init() {
+	client.SetTimeout(timeoutSeconds * time.Second)
+}
+
 type accessToken struct {
 	AccessToken string `json:"access_token"`
 	UserId      int64  `json:"user_id"`
 	ClientId    int64  `json:"client_id"`
 	Expires     int64  `json:"expires"`
-}
-
-type oauthInterface interface {
-}
-
-type oauthClient struct {
 }
 
 func IsPublic(r *http.Request) bool {
@@ -46,22 +44,69 @@ func IsPublic(r *http.Request) bool {
 	return r.Header.Get(headerXPublic) == "true"
 }
 
+func GetCallerId(r *http.Request) int64 {
+	if r == nil {
+		return 0
+	}
+	c, err := strconv.ParseInt(r.Header.Get(headerXCallerId), 10, 64)
+	if err != nil {
+		return 0
+	}
+	return c
+}
+
+func GetClientId(r *http.Request) int64 {
+	if r == nil {
+		return 0
+	}
+	c, err := strconv.ParseInt(r.Header.Get(headerXClientId), 10, 64)
+	if err != nil {
+		return 0
+	}
+	return c
+}
+
 func AuthenticateRequest(r *http.Request) *errors.RestErr {
 	if r == nil {
 		return nil
 	}
-	at := strings.TrimSpace(r.URL.Query().Get(paramAccessToken))
-	if at == "" {
+	cleanRequest(r)
+	ats := strings.TrimSpace(r.URL.Query().Get(paramAccessToken))
+	if ats == "" {
 		return nil
 	}
+	at, err := getAccessToken(ats)
+	if err != nil {
+		return err
+	}
+	r.Header.Add(headerXCallerId, strconv.FormatInt(at.UserId, 10))
+	r.Header.Add(headerXClientId, strconv.FormatInt(at.ClientId, 10))
 	return nil
 }
 
-func getAccessToken(at string) (*accessToken, *errors.RestErr) {
-	resp, err := client.R().SetHeader("Content-Type",UserContentType).Get(UserBaseURI + UserURI + "/" + at)
+func cleanRequest(request *http.Request) {
+	if request == nil {
+		return
+	}	
+	request.Header.Del(headerXClientId)
+	request.Header.Del(headerXCallerId)
+}
+
+func getAccessToken(ats string) (*accessToken, *errors.RestErr) {
+	re, err := client.R().SetHeader("Content-Type",UserContentType).Get(UserBaseURI + UserURI + "/" + ats)
 	if err != nil {
 		return nil, errors.NewInternalServerError("Authentication Service off")
 	}
-	return nil, nil
-
+	if re.RawResponse.StatusCode > 299  {
+		var rErr errors.RestErr
+		if err := json.Unmarshal(re.Body(), &rErr); err != nil {
+			return nil, errors.NewInternalServerError("Invalid rest-client error unmarshall client")
+		}
+		return nil, &rErr
+	}
+	var at accessToken
+	if err := json.Unmarshal(re.Body(), &at); err != nil {
+		return nil, errors.NewInternalServerError("Invalid rest-client access token unmarshall client")
+	}
+	return &at, nil
 }
